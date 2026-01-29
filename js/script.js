0bded4734a7428892fd86a35df2140b1b1631306
“Calixto” <l19070031@huetamo.tecnm.mx>
2026-01-29 15:21:17 -0600

    Ajustes en tag metadata y js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-import { features, hunting, evolution, latestData } from "./data.js"
+import { features, hunting, evolution, latestData } from "./data.js";
 
 
 // links
-let links = document.querySelectorAll('span')
-let titleDocument = document.getElementById('title')
-let paragraphDocument = document.getElementById('paragraph')
+let links = document.querySelectorAll('span');
+let titleDocument = document.getElementById('title');
+let paragraphDocument = document.getElementById('paragraph');
 
 
 //Add a element click for each link
 links.forEach(function (link) {
     link.addEventListener('click', function () {
         links.forEach(function (link) {
-            link.classList.remove('active') //Removing the class active of link
+            link.classList.remove('active'); //Removing the class active of link
         });
 
-        this.classList.add('active')
+        this.classList.add('active');
 
         // Obtain the values of the function  getContent
-        let content = getContent(this.textContent)
+        let content = getContent(this.textContent);
 
-        titleDocument.innerHTML = content.title
-        paragraphDocument.innerHTML = content.paragraph
+        titleDocument.innerHTML = content.title;
+        paragraphDocument.innerHTML = content.paragraph;
 
     });
 
